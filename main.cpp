d4841845ba56dd0cf9683a2837989a63dba3eea9
Nelson I. Gonzalez <nigm2005@gmail.com>
2016-05-23 23:54:49 -0500

    Changing run to save in disk

--- main.cpp
+++ main.cpp
@@ -1,34 +1,36 @@
 #include <iostream>
 #include <fstream>
-#include "empirical.hpp"
-//#include "mx_vision.h"
+#include <string>
+#include "mx_vision.h"
 
-//unsigned char image[BUFFER_SIZE];
+unsigned char image[BUFFER_SIZE];
+std::string files[] = {
+    "/data/aff2957cd4f5be4f5ac539724456b2e465859c097d068b32e08c0927641483df",
+    "/data/83f55ddcc260177b10ad123d56a52bb63e25464e0b7b1e03490dc46b422607f2",
+    "/data/ce5903e2cfd52b4241f03e0665e9fff081d7ca104e438f70ccf34b2c05c58dad"
+};
 
 int main() {
-    /*mx_vision_init();
-    std::ifstream file("record2");
-    int byte = 0;
-    int counter = 0;
-    while (file >> byte) {
-        unsigned char charByte = (unsigned char)byte;
-        image[counter] = charByte;
-        counter++;
-        if (counter == BUFFER_SIZE) {
-            mx_vision_see(image);
-            std::cout << "Reds detected " << redsDetected << std::endl;
-            for (int i = 0; i < redsDetected; ++i) {
-                std::cout << "Object " << reds[i].id << " red at distance " << reds[i].dis << " direction " << reds[i].dir << std::endl;
+    int my_reds = 0;
+    mx_vision_init();
+    for (int i = 0; i < 3; i++) {
+        std::ifstream file(files[i]);
+        int byte = 0;
+        int counter = 0;
+        while (file >> byte) {
+            unsigned char charByte = (unsigned char)byte;
+            image[counter] = charByte;
+            counter++;
+            if (counter == BUFFER_SIZE) {
+                mx_vision_see(image);
+                my_reds += redsDetected;
+                counter = 0;
             }
-            std::cout << "Blues detected " << bluesDetected << std::endl;
-            for (int i = 0; i < bluesDetected; ++i) {
-                std::cout << "Object " << blues[i].id << " blue at distance " << blues[i].dis << " direction " << blues[i].dir << std::endl;
-            }
-            counter = 0;
-            std::cout << std::endl;
         }
+        file.close();
     }
-    file.close();*/
-    Empirical::post("www.google.com");
+    std::ofstream json("/workspace/results.json", std::ofstream::out);
+    json << "{ overall: { metric: 'count', value: " << my_reds << " } }" << std::endl;
+    json.close();
     return 0;
 }
